03aa02051f77f0a4fdad14c05287d6c9c6c91b1b
sdz123 <shidezhi123@gmail.com>
2023-07-18 16:02:22 +0800

    init

---
+++ js/src/constants/music.js
@@ -0,0 +1,13 @@
+export const music = [
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334778/music/s15bb9xge91rqk976zs3.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334549/music/h8zvvrlpctc21mfutguz.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334548/music/eyumtfiia8zm4evdrioh.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334540/music/qepntkgueijajzqtyclo.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334538/music/a2vzmrtsylohnrwzefdj.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334531/music/vb26kizebyadiauovkmi.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334530/music/mdecshzpt9o2vsrl7gkl.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334524/music/mrbowliiv7ybkuksjoty.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334518/music/pedexs1w7tuf2adxtnxr.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334515/music/pj9xwgxfekhsouv2rlfp.mp3",
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334514/music/omqufhl8vyctzsy5u9gq.mp3"
+]
