f8be5e242f475fbbebaf694ba4dad99bf915942f
sdz123 <shidezhi123@gmail.com>
2024-07-12 11:18:47 +0800

    Update m.js

--- js/src/m.js
+++ js/src/m.js
@@ -1,18 +1,16 @@
 export default [
-   "https://res.cloudinary.com/dk1dkencm/video/upload/v1690454461/music/bru7odhqcaguwfg17org.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1690367413/music/nwwp1pminfdvcr4awhtz.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1690366584/music/afbhodujejp3px3wmvqe.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1689763114/music/xi05uopy5pzhvo9tywfa.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334778/music/s15bb9xge91rqk976zs3.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334549/music/h8zvvrlpctc21mfutguz.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334548/music/eyumtfiia8zm4evdrioh.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334540/music/qepntkgueijajzqtyclo.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334538/music/a2vzmrtsylohnrwzefdj.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334531/music/vb26kizebyadiauovkmi.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334530/music/mdecshzpt9o2vsrl7gkl.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334524/music/mrbowliiv7ybkuksjoty.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334518/music/pedexs1w7tuf2adxtnxr.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334515/music/pj9xwgxfekhsouv2rlfp.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334514/music/omqufhl8vyctzsy5u9gq.mp3",
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1692080345/music/xqcqluwp9cgalbupztlo.mp3"
+   "https://res.cloudinary.com/dk1dkencm/video/upload/v1690454461/music/bru7odhqcaguwfg17org.mp3", // 关于小熊
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1690367413/music/nwwp1pminfdvcr4awhtz.mp3", // 危险派对 王以太
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1690366584/music/afbhodujejp3px3wmvqe.mp3", // 威远故事
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1689763114/music/xi05uopy5pzhvo9tywfa.mp3", // rap star 
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334549/music/h8zvvrlpctc21mfutguz.mp3", // 九局下半
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334548/music/eyumtfiia8zm4evdrioh.mp3", // 空壳
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334538/music/a2vzmrtsylohnrwzefdj.mp3",// tupac
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334531/music/vb26kizebyadiauovkmi.mp3",// 八贼 master
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334530/music/mdecshzpt9o2vsrl7gkl.mp3",//  说错入门2.0
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334524/music/mrbowliiv7ybkuksjoty.mp3", // 淫灭辉飞
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334518/music/pedexs1w7tuf2adxtnxr.mp3",// 恨与爱
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334515/music/pj9xwgxfekhsouv2rlfp.mp3",//通俗易懂diss
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334514/music/omqufhl8vyctzsy5u9gq.mp3",// 崂山道士
+    "https://res.cloudinary.com/dk1dkencm/video/upload/v1692080345/music/xqcqluwp9cgalbupztlo.mp3",// 从什么时候开始 秋刀鱼会过期。。
 ]
