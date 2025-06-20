35126c95a1cea5d94eca8f9a467d9ef2e5fc5811
sdz123 <shidezhi123@gmail.com>
2025-06-20 15:11:08 +0800

    添加注释

--- js/src/m.js
+++ js/src/m.js
@@ -17,3 +17,8 @@ export default [
    "https://res.cloudinary.com/dk1dkencm/video/upload/v1720754961/music/kvby36tx9szst58bab4n.mp3",// 工厂
    "https://res.cloudinary.com/dk1dkencm/video/upload/v1750401219/music/yinguo.mp3" // 因果
 ]
+// 更新音乐流程：
+//1.在网上把想下载的音乐down下来，然后把down下来的文件上传到https://console.cloudinary.com/console/c-c66861e0402ca5ab806f0cbd5e8dc9/media-explorer/music云平台
+//2.上传到云平台后你会得到一个音乐的地址，把这个地址复制到m.js文件中
+//3.运行npm run build打包一下文件，把产生的所有修改（包括m.js文件）commit一下，然后push到github上(用命令操作)
+//4.完成第三步之后你就不用管了，因为github监听到这个仓库的push会自动部署。但如果你不放心可以打开github的sdz123.github.io，点击actions看查看部署结果
