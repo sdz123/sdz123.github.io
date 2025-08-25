f7a3cce93b4e316c7feac63bf49530e4c6f8ae03
sdz123 <shidezhi123@gmail.com>
2025-08-25 15:39:43 +0800

    Update m.js

--- js/src/m.js
+++ js/src/m.js
@@ -1,13 +1,10 @@
 export default [
    "https://res.cloudinary.com/dk1dkencm/video/upload/v1690454461/music/bru7odhqcaguwfg17org.mp3", // 关于小熊
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1690367413/music/nwwp1pminfdvcr4awhtz.mp3", // 危险派对 王以太
     "https://res.cloudinary.com/dk1dkencm/video/upload/v1690366584/music/afbhodujejp3px3wmvqe.mp3", // 威远故事
     "https://res.cloudinary.com/dk1dkencm/video/upload/v1689763114/music/xi05uopy5pzhvo9tywfa.mp3", // rap star 
     "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334549/music/h8zvvrlpctc21mfutguz.mp3", // 九局下半
     "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334548/music/eyumtfiia8zm4evdrioh.mp3", // 空壳
     "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334538/music/a2vzmrtsylohnrwzefdj.mp3",// tupac
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334531/music/vb26kizebyadiauovkmi.mp3",// 八贼 master
-    "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334530/music/mdecshzpt9o2vsrl7gkl.mp3",//  说错入门2.0
     "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334524/music/mrbowliiv7ybkuksjoty.mp3", // 淫灭辉飞
     "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334518/music/pedexs1w7tuf2adxtnxr.mp3",// 恨与爱
     "https://res.cloudinary.com/dk1dkencm/video/upload/v1687334515/music/pj9xwgxfekhsouv2rlfp.mp3",//通俗易懂diss
