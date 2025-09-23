a0fd6b0a0479fd08b21ee0a5ff255e0eae44a9f6
sdz123 <shidezhi123@gmail.com>
2025-09-23 15:55:03 +0800

    新增音乐

--- js/src/playLists.js
+++ js/src/playLists.js
@@ -29,8 +29,17 @@ export default [
         cover: "",
         tracks: [
             { id: "qingfeng", title: "酒馆打样", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758611453/music/douyin/%E4%BD%A0%E7%9A%84%E9%85%92%E9%A6%86%E5%AF%B9%E6%88%91%E6%89%93%E4%BA%86%E7%83%8A_-_%E9%99%88%E9%9B%AA%E5%87%9D_g9voet.mp3" },
-           ,
-           { id: "lvse", title: "绿色", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758611398/music/douyin/%E7%BB%BF%E8%89%B2_-_%E9%99%88%E9%9B%AA%E5%87%9D_syp0pa.mp3" },
+            { id: "lvse", title: "绿色", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758611398/music/douyin/%E7%BB%BF%E8%89%B2_-_%E9%99%88%E9%9B%AA%E5%87%9D_syp0pa.mp3" },
+            { id: "langlang", title: "啷哩啷", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758611338/music/douyin/%E5%95%B7%E4%B8%AA%E5%93%A9%E4%B8%AA%E5%95%B7_-_%E9%B9%8F%E6%B3%8A_lx72sr.flac" },
+            { id: "sarilang", title: "萨日朗", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758611184/music/douyin/%E7%81%AB%E7%BA%A2%E7%9A%84%E8%90%A8%E6%97%A5%E6%9C%97_-_%E8%A6%81%E4%B8%8D%E8%A6%81%E4%B9%B0%E8%8F%9C_visgwi.mp3" },
+            { id: "keyi", title: "可不可以", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758611127/music/douyin/%E5%8F%AF%E4%B8%8D%E5%8F%AF%E4%BB%A5_-_%E5%BC%A0%E7%B4%AB%E8%B1%AA_lnppix.mp3" },
+            { id: "mangzhong", title: "芒种", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758611011/music/douyin/%E8%8A%92%E7%A7%8D_%E5%9B%BD%E8%AF%AD_-_%E9%9F%B3%E9%98%99%E8%AF%97%E5%90%AC_%E8%B5%B5%E6%96%B9%E5%A9%A7_yyxqfw.mp3" },
+            { id: "jiangzhende", title: "讲真的", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758610964/music/douyin/%E8%AE%B2%E7%9C%9F%E7%9A%84_-_%E6%9B%BE%E6%83%9C_syrmfg.mp3" },
+            { id: "shijainmeihao", title: "世间美好", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758612992/music/douyin/%E6%9F%8F%E6%9D%BE_-_%E4%B8%96%E9%96%93%E7%BE%8E%E5%A5%BD%E8%88%87%E4%BD%A0%E7%92%B0%E7%92%B0%E7%9B%B8%E6%89%A3_%E6%AD%A4%E6%99%82%E5%B7%B2%E9%B6%AF%E9%A3%9B%E8%8D%89%E9%95%B7_%E6%84%9B%E7%9A%84%E4%BA%BA%E6%AD%A3%E5%9C%A8%E8%B7%AF%E4%B8%8A_%E5%8B%95%E6%85%8B%E6%AD%8C%E8%A9%9ELyrics_-_EHPMusicChannel_rnm6vr.mp3" },
+            { id: "baiyueguang", title: "白月光", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758610817/music/douyin/%E7%99%BD%E6%9C%88%E5%85%89%E4%B8%8E%E6%9C%B1%E7%A0%82%E7%97%A3_-_%E5%A4%A7%E7%B1%BD_tt8jgd.mp3" },
+            { id: "shanle", title: "删了吧", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758612621/music/douyin/%E5%88%AA%E4%BA%86%E5%90%A7-%E7%85%99_%E8%A8%B1%E4%BD%B3%E8%B1%AA_%E8%A6%81%E4%B8%8D%E4%BD%A0%E9%82%84%E6%98%AF%E6%8A%8A%E6%88%91%E5%88%AA%E4%BA%86%E5%90%A7_%E6%88%91%E5%92%AC%E7%B7%8A%E7%89%99%E9%97%9C%E5%91%BD%E4%BB%A4%E6%88%91%E7%99%BC%E5%87%BA%E9%80%99%E5%8F%A5%E8%A9%B1_%E7%84%A1%E6%90%8D%E9%9F%B3%E6%A8%82_%E5%8B%95%E6%85%8B%E6%AD%8C%E8%A9%9EMV_-_Treasure_Music_inzg6c.mp3" },
+            { id: "xiancheng", title: "没有县城能行吗？", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758613553/music/douyin/%E6%B2%A1%E6%9C%89%E5%8E%BF%E5%9F%8E_%E4%B8%87%E4%B8%87%E4%B8%8D%E8%83%BD_-_Surmon_s_wild_ride_ldrhx4.mp3" },
+            { id: "libiekaihua", title: "离别开花", artist: "", url: "https://res.cloudinary.com/dk1dkencm/video/upload/v1758614070/music/douyin/%E9%9B%A2%E5%88%A5%E9%96%8B%E5%87%BA%E8%8A%B1_-_%E5%B0%B1%E6%98%AF%E5%8D%97%E6%96%B9%E5%87%B1_%E5%8A%A8%E6%80%81%E6%AD%8C%E8%AF%8D_Lyrics_Video_%E5%BD%93%E7%A6%BB%E5%88%AB%E5%BC%80%E5%87%BA%E8%8A%B1_%E4%BC%B8%E5%87%BA%E6%96%B0%E9%95%BF%E7%9A%84%E6%9E%9D%E6%A1%A0_%E5%83%8F%E5%86%AC%E5%8E%BB%E6%98%A5%E5%8F%88%E6%9D%A5_%E7%AD%89%E5%BE%85%E5%BF%83%E9%9B%AA%E8%9E%8D%E5%8C%96_-_KUNmusic_wt6lvu.mp3" },
 
             // ...把属于流行的继续往这里放
         ]
